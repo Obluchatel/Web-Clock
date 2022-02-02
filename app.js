1346111808cbe4c001bf13051c8331f3cef7e3f4
Obluchatel <azamatovartem@gmail.com>
2022-02-02 19:56:38 +0100

    added date

--- app.js
+++ app.js
@@ -29,9 +29,23 @@ function Time() {
     minute = update(minute);
     second = update(second);
     // Adding time elements to the div
+    
     document.getElementById("digital-clock").innerText = hour + " : " + minute + " : " + second + " " + period;
     // Set Timer to 1 sec (1000 ms)
     setTimeout(Time, 1000);
+    // adding the date to clock
+    var date = new Date();
+    var day = date.getDate();
+    var year = date.getFullYear();
+    var mounth = date.getUTCMonth();
+    if (mounth <= 11) {
+        mounth = mounth+1;
+    }
+     else {
+        mounth;
+    }
+    document.getElementById("date").innerText = day +"."+ mounth + "." + year;
+    setTimeout(dayoftheweek, 1000);
    }
     // Function to update time elements if they are less than 10
     // Append 0 before time elements if they are less than 10
@@ -53,4 +67,21 @@ function darkMode() {
     div.classList.toggle("dark");
     const button = document.querySelector("#button")
     button.classList.toggle("dark")
+    const date = document.querySelector("#date")
+    date.classList.toggle("dark")
 }
+// function dayoftheweek() {
+//     // var date = new Date();
+//     // var day = date.getDate();
+//     // var year = date.getFullYear();
+//     // var mounth = date.getUTCMonth();
+//     // if (mounth <= 11) {
+//     //     mounth = mounth+1;
+//     // }
+//     //  else {
+//     //     mounth;
+//     // }
+//     // document.getElementById("date").innerText = day +"."+ mounth + "." + year;
+//     // setTimeout(dayoftheweek, 1000);
+// }
+dayoftheweek();
